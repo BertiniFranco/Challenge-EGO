97aff451d83842d347e6d794bf63f988f38c3979
Franco <bertini0709@gmail.com>
2024-03-11 12:07:53 -0300

    Local settings change

--- ego_backend/ego_backend/settings.py
+++ ego_backend/ego_backend/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'backend_challenge_ego',
         'USER': 'root',
-        'PASSWORD': '1234',
+        'PASSWORD': 'YOUR_PASSWORD',
         'HOST': 'localhost',
         'PORT': '3306'
     }
--- ego_frontend/src/dataAccess/DataAccess.js
+++ ego_frontend/src/dataAccess/DataAccess.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/ego/';
+const API_URL = 'YOUR_API_URL';
 
 class DataAccess {
     response = null;
